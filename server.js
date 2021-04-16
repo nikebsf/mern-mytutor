1634474d7f8cee3c2602e7dcd64f70804a7b4bdf
Nikebsf <nmpbsf@gmail.com>
2021-04-16 02:34:44 -0400

    Added voice input
    Tried to add voice input in both input fields but could not implemented. just in first input :(

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const config = require("config");
 const cors = require("cors");
 
 const app = express();
 
-// bodyparser middleware
-// BodyParse is built into Express js So now you don't have to install body-parser, do this instead
-//app.use(bodyParser.json());
-app.use(cors());
 app.use(express.json());
+app.use(cors());
+const db = config.get("mongoURI");
 
-// DB config
-const db = require("./config/keys").mongoURI;
-
-//connect to mongodb
 mongoose
 	.connect(db, { useNewUrlParser: true, useCreateIndex: true })
 	.then(() => console.log("MongoDB Connected.."))
 	.catch((err) => console.log(err));
 
-// use routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profs", require("./routes/api/profs"));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started on port: ${port}`));
